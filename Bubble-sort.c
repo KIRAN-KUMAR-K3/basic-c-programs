b9a03128527f590a89f72fe43cbbec755b021aaf
KIRAN KUMAR K <137183954+KIRAN-KUMAR-K3@users.noreply.github.com>
2023-11-21 22:40:56 +0530

    Update Bubble-sort.c

--- Bubble-sort.c
+++ Bubble-sort.c
@@ -5,6 +5,7 @@ void swap(int *xp, int *yp) {
     *xp = *yp;
     *yp = temp;
 }
+
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -14,25 +15,48 @@ void bubbleSort(int arr[], int n) {
         }
     }
 }
+
 void printArray(int arr[], int size) {
-    for (int i=0; i < size; i++) {
+    printf("Sorted Array: ");
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
+
 int main() {
     int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    int arr[n];
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+
+    printf("Welcome to the Bubble Sort Program\n");
+    printf("Enter the number of elements in the array: ");
+    
+    // Input validation for the number of elements
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid input for the number of elements. Please enter a positive integer.\n");
+        return 1;  // Exit with an error code
     }
-    printf("Original array: \n");
-    printArray(arr, n);
+
+    int arr[n];
+
+    printf("Enter %d elements, separated by spaces:\n", n);
+
+    // Input validation for array elements
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input for element %d. Please enter integers only.\n", i + 1);
+            return 1;  // Exit with an error code
+        }
+    }
+
+    printf("\nOriginal Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     bubbleSort(arr, n);
-    printf("Sorted array: \n");
+
     printArray(arr, n);
+
     return 0;
 }
