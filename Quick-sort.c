b44a2630b8b52a856436bfcff906828a9f8213a2
KIRAN KUMAR K <137183954+KIRAN-KUMAR-K3@users.noreply.github.com>
2023-11-21 22:42:33 +0530

    Create Quick-sort.c

---
+++ Quick-sort.c
@@ -0,0 +1,71 @@
+Certainly! Here's a simple implementation of the Quick Sort algorithm in C:
+
+```c
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    printf("Sorted Array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nOriginal Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    quickSort(arr, 0, n - 1);
+
+    printArray(arr, n);
+
+    return 0;
+}
+```
+
+This program uses the Quick Sort algorithm to sort an array of integers. It takes the number of elements and the array as input from the user, prints the original array, performs the quick sort, and then prints the sorted array.
