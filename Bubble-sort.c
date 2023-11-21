f8362f5649fa49b8f5d47bce1a1856a71fd34dce
KIRAN KUMAR K <137183954+KIRAN-KUMAR-K3@users.noreply.github.com>
2023-11-21 22:37:12 +0530

    Update Bubble-sort.c

--- Bubble-sort.c
+++ Bubble-sort.c
@@ -5,6 +5,7 @@ void swap(int *xp, int *yp) {
     *xp = *yp;
     *yp = temp;
 }
+
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -14,19 +15,34 @@ void bubbleSort(int arr[], int n) {
         }
     }
 }
+
 void printArray(int arr[], int size) {
     for (int i=0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
+
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     printf("Original array: \n");
     printArray(arr, n);
+
     bubbleSort(arr, n);
+
     printf("Sorted array: \n");
     printArray(arr, n);
+
     return 0;
 }
