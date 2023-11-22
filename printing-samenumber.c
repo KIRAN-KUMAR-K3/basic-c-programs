122752c9eaa7568c7441c954997fea07adf08f7a
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-11-22 15:37:58 +0530

    modification of programm codes

--- printing-samenumber.c
+++ printing-samenumber.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, j;
+void printNumberPattern(int n) {
+    int i, j;
 
-    // Get the value of n from the user
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-
-    // Print the pattern
+    // Display the number pattern for the given value of n
+    printf("Number pattern for n = %d:\n", n);
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n; j++) {
             printf("%d", i);
         }
         printf("\n");
     }
+}
+
+int main() {
+    int n;
+
+    // Get the value of n from the user
+    printf("Enter the value of n for the number pattern: ");
+    scanf("%d", &n);
+
+    // Validate the input
+    if (n <= 0) {
+        printf("Invalid input. Please enter a positive integer for n.\n");
+        return 1;  // Return an error code
+    }
+
+    // Display the number pattern
+    printNumberPattern(n);
 
     return 0;
 }
