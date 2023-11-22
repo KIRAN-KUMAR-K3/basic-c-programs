950f2ccc20eeeb0a7f515e6de71f3875be11cb16
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-11-22 15:31:45 +0530

    some new changes in programm codes

--- pritn-in-triangle-order.c
+++ pritn-in-triangle-order.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, j;
+void printTriangle(int n) {
+    int i, j;
 
-    // Get the value of n from the user
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
+    printf("Pattern for n = %d:\n", n);
 
-    // Print the pattern in a triangle shape
+    // Print the pattern in a triangle shape with '1' at the middle
     for (i = 1; i <= n; i++) {
         // Print spaces for alignment
         for (j = 1; j <= n - i; j++) {
             printf(" ");
         }
 
-        // Print consecutive numbers
+        // Print consecutive numbers in ascending order
         for (j = 1; j <= i; j++) {
             printf("%d", j);
         }
 
-        // Print consecutive numbers in reverse order
+        // Print consecutive numbers in reverse order (excluding '1' for the middle row)
         for (j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
 
         printf("\n");
     }
+}
+
+int main() {
+    int n;
+
+    // Get the value of n from the user
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Validate the input
+    if (n <= 0) {
+        printf("Please enter a positive integer for n.\n");
+        return 1;  // Return an error code
+    }
+
+    // Print the triangle pattern
+    printTriangle(n);
 
     return 0;
 }
